e1f2db7eaed2799cfa666e23da61653e99f80be1
Joe Regan <joer80@gmail.com>
2026-02-21 18:31:12 -0600

    change welcome to home

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::view('/', 'home')->name('home');
 
 Route::view('about', 'about')->name('about');
 
