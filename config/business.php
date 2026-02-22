823e55dc4c75cdf5fd9e89390bba52dbb2dd5a36
Joe Regan <joer80@gmail.com>
2026-02-21 18:26:29 -0600

    refactor: remove placeholder fallbacks from business config

--- config/business.php
+++ config/business.php
@@ -13,15 +13,15 @@ return [
     |
     */
 
-    'phone' => env('BUSINESS_PHONE', '+1 (555) 123-4567'),
+    'phone' => env('BUSINESS_PHONE'),
 
-    'email' => env('BUSINESS_EMAIL', 'hello@getrows.com'),
+    'email' => env('BUSINESS_EMAIL'),
 
     'address' => [
-        'street' => env('BUSINESS_ADDRESS_STREET', '123 Maple Street, Suite 400'),
-        'city_state_zip' => env('BUSINESS_ADDRESS_CITY_STATE_ZIP', 'Austin, TX 78701'),
+        'street' => env('BUSINESS_ADDRESS_STREET'),
+        'city_state_zip' => env('BUSINESS_ADDRESS_CITY_STATE_ZIP'),
     ],
 
-    'hours' => env('BUSINESS_HOURS', 'Monday – Friday, 9am – 5pm CT'),
+    'hours' => env('BUSINESS_HOURS'),
 
 ];
