a24acef7f228e42ec171be6caa7fbfad1f37ca4c
Joe Regan <joer80@gmail.com>
2026-03-04 14:10:50 -0600

    add generator meta tag for WebProCMS discoverability

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="generator" content="WebProCMS" />
 
 <title>{{ $title ?? config('app.name') }}</title>
 
