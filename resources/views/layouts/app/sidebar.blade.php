eb45a0103ae4494a8eba88549a9a11c8c1b7490e
Joe Regan <joer80@gmail.com>
2026-03-05 08:57:04 -0600

    Dont preload editor for standard user

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
-        <link rel="preload" href="{{ Vite::asset('resources/css/editor.css') }}" as="style">
+        @if (auth()->user()->isAtLeast(\App\Enums\Role::Manager))
+            <link rel="preload" href="{{ Vite::asset('resources/css/editor.css') }}" as="style">
+        @endif
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
