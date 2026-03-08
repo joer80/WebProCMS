0946657b4cce5068930b3d1be10847972aba89cb
Joe Regan <joer80@gmail.com>
2026-03-08 16:36:45 -0500

    Add a lazy load checkbox to the media group

--- resources/views/components/dl/media.blade.php
+++ resources/views/components/dl/media.blade.php
@@ -5,11 +5,12 @@ $wrapperCls = content($slug, 'image_wrapper_classes', $defaultWrapperClasses);
 $imageCls = content($slug, 'image_classes', $defaultImageClasses);
 $imgSrc = content($slug, 'image', $defaultImage);
 $imgAlt = content($slug, 'image_alt', '');
+$imgLazy = content($slug, 'toggle_image_lazy', '');
 @endphp
 @if(content($slug, 'toggle_image', '1'))
 <div class="{{ $wrapperCls }}" data-editor-group="media">
     @if($imgSrc)
-        <img src="{{ $imgSrc }}" alt="{{ $imgAlt }}" class="{{ $imageCls }}">
+        <img src="{{ $imgSrc }}" alt="{{ $imgAlt }}" class="{{ $imageCls }}"@if($imgLazy) loading="lazy"@endif>
     @else
         <span class="text-zinc-400 dark:text-zinc-500 text-sm">Image / Video</span>
     @endif
