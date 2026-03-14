c719af9540123099f85c437ca24ec932b480bac9
Joe Regan <joer80@gmail.com>
2026-03-14 14:06:03 -0500

    more logo fallbacks

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -5,13 +5,13 @@
 @if($sidebar)
     <flux:sidebar.brand {{ $attributes }}>
         <x-slot name="logo">
-            <img src="{{ \App\Models\Setting::get('branding.logo_url', asset('images/logo.svg')) }}" alt="{{ config('app.name') }}" class="h-8 w-auto" />
+            <img src="{{ \App\Models\Setting::get('branding.logo_url') ?: asset('images/logo.svg') }}" alt="{{ config('app.name') }}" class="h-8 w-auto" />
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:brand {{ $attributes }}>
         <x-slot name="logo">
-            <img src="{{ \App\Models\Setting::get('branding.logo_url', asset('images/logo.svg')) }}" alt="{{ config('app.name') }}" class="h-8 w-auto" />
+            <img src="{{ \App\Models\Setting::get('branding.logo_url') ?: asset('images/logo.svg') }}" alt="{{ config('app.name') }}" class="h-8 w-auto" />
         </x-slot>
     </flux:brand>
 @endif
